44134709ed9f3c58c5f6931c6aa5b2f5b6f4e554
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-09-07 19:03:21 -0400

    docs

--- src/main/kotlin/io/foxcapades/lib/cli/builder/component/CliComponentAnnotation.kt
+++ src/main/kotlin/io/foxcapades/lib/cli/builder/component/CliComponentAnnotation.kt
@@ -1,5 +1,14 @@
 package io.foxcapades.lib.cli.builder.component
 
+/**
+ * Base type representing a CLI component annotation instance.
+ *
+ * @since 1.0.0
+ */
 interface CliComponentAnnotation {
+
+  /**
+   * Indicates whether the annotation instance was marked as being required.
+   */
   val required: Boolean
 }
